ba11c214d51ec87bb848b320a39bfeef022bf559
shekharm-barclays <shekhar.marne@barclays.com>
2023-04-18 03:27:44 -0700

    First Commit

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "3fb59932-3429-4e93-8467-7cb7c3e850f0";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "4a6182ce-1ae6-4f8d-8ade-4252bdc08a1a";
+}
